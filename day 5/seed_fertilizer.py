46ecc2645ee3b173fb7ac4f81fb6f65940f2fb69
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-05 13:48:23 +0530

    Add slightly optimized brute force for day 5

--- day 5/seed_fertilizer.py
+++ day 5/seed_fertilizer.py
@@ -3,14 +3,14 @@ def get_puzzle_input(directory):
         file = file.read().split("\n\n")
     
     seeds = get_seeds(file[0])
-    mappings = get_mappings(file[1:])
+    mappings = get_all_mappings(file[1:])
     return seeds, mappings
 
 def get_seeds(seeds):
     seeds = seeds.split(": ")[1].split(" ")
     return list(map(int, seeds))
 
-def get_mappings(mappings):
+def get_all_mappings(mappings):
     MAPPINGS = dict()
     for mapping in mappings:
         mapping = mapping.split("\n")
@@ -32,16 +32,6 @@ def get_mapping(source, destination, source_number):
             return req_map[destination] + diff
     return source_number
 
-def get_location_number(seed):
-    soil = get_mapping("seed", "soil", seed)
-    fertilizer = get_mapping("soil", "fertilizer", soil)
-    water = get_mapping("fertilizer", "water", fertilizer)
-    light = get_mapping("water", "light", water)
-    temperature = get_mapping("light", "temperature", light)
-    humidity = get_mapping("temperature", "humidity", temperature)
-    location = get_mapping("humidity", "location", humidity)
-    return location
-
 def get_seed_number(location):
     humidity = get_mapping("location", "humidity", location) 
     temperature = get_mapping("humidity", "temperature", humidity)
@@ -52,21 +42,23 @@ def get_seed_number(location):
     seed = get_mapping("soil", "seed", soil)
     return seed
 
-def check_seed_exists(seed_number):
+def check_seed_exists(seed_number, discrete=False):
+    if discrete:
+        return seed_number in SEEDS
     for index in range(0, len(SEEDS), 2):
         if SEEDS[index] <= seed_number < SEEDS[index] + SEEDS[index + 1]:
             return True
     return False
 
-def get_best_seed_location():
-    lowest_location = 0 # change 52390725
-    while not check_seed_exists(get_seed_number(lowest_location)):
+def get_best_seed_location(discrete=False):
+    lowest_location = 0 # change magnitudes
+    while not check_seed_exists(get_seed_number(lowest_location), discrete):
         lowest_location += 1
     return lowest_location
 
-def get_lowest_location_number(seeds):
-    return min([get_location_number(seed) for seed in seeds])
+SEEDS, MAPPINGS = get_puzzle_input(r"./puzzle_input.txt")
 
-SEEDS, MAPPINGS = get_puzzle_input(r"./input.txt")
-# print(get_lowest_location_number(SEEDS))
-print(get_best_seed_location())
+# part a
+print(get_best_seed_location(True))
+# part b
+print(get_best_seed_location(False))
