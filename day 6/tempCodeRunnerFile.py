ebefd59f24076bad6cfd4812c367f9f525b19a22
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-06 16:51:19 +0530

    Delete unnecessary file

--- day 6/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-    lesser = 0
-    for holding_time in range(0, time + 1):
-        lesser += 1
-        if holding_time * (time - holding_time) > distance:
-            lesser -= 1
-            break
