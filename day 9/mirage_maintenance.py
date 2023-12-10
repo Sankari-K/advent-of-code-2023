f5ba4e484c552553ed1e776889efaed1d2ea0596
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-10 10:15:23 +0530

    Add better soln for day 9

--- day 9/mirage_maintenance.py
+++ day 9/mirage_maintenance.py
@@ -5,16 +5,14 @@ def get_puzzle_input(directory):
 def get_difference(history):
     return [b - a for a, b in zip(history, history[1:])]
 
-def get_extrapolated_value(history, forwards):
+def get_extrapolated_value(history):
     if set(history) == {0}:
         return 0
-    if forwards:
-        return history[-1] + get_extrapolated_value(get_difference(history), forwards)
-    return history[0] - get_extrapolated_value(get_difference(history), forwards)
+    return history[-1] + get_extrapolated_value(get_difference(history))
 
-def get_sum_extrapolated_values(REPORT, forwards=True):
-    return sum([get_extrapolated_value(history, forwards) for history in REPORT])
+def get_sum_extrapolated_values(REPORT):
+    return sum([get_extrapolated_value(history) for history in REPORT])
 
 REPORT = get_puzzle_input(r"./puzzle_input.txt")
 print(get_sum_extrapolated_values(REPORT))
-print(get_sum_extrapolated_values(REPORT, False))
+print(get_sum_extrapolated_values(history[::-1] for history in REPORT))
