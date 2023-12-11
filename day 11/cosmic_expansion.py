259bdbe6031da4df16a45abc7f6b5fdf6e4f2c08
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-11 22:32:05 +0530

    Add solution for day 11

--- day 11/cosmic_expansion.py
+++ day 11/cosmic_expansion.py
@@ -11,36 +11,37 @@ def get_empty_spaces(image):
         if set(row) == set("."):
             rows.add(index)
             
-    for index, col in enumerate([list(i) for i in zip(*image)]):
+    for index, col in enumerate([i for i in zip(*image)]):
         if set(col) == set("."):
             cols.add(index)
 
     return rows, cols
 
-def get_galaxies(image, empty_rows, empty_cols, offset):
+def get_galaxies(universe, empty_rows, empty_cols, expansion_rate):
     galaxies = []
     row_offset = 0
-    for x, row in enumerate(image):
+    for x, row in enumerate(universe):
         if x in empty_rows:
-            row_offset += offset - 1
+            row_offset += expansion_rate - 1
+            
         col_offset = 0
         for y, point in enumerate(row):
             if y in empty_cols:
-                col_offset += offset - 1
+                col_offset += expansion_rate - 1
             if point == "#":
                 galaxies.append([x + row_offset, y + col_offset])
     return galaxies
 
 def get_sum_distances(GALAXIES):
     distances = 0
-    for a, b in itertools.combinations(GALAXIES, 2):
-        distances += abs(a[0] - b[0]) + abs(a[1] - b[1])
+    for (ax, ay), (bx, by) in itertools.combinations(GALAXIES, 2):
+        distances += abs(ax - bx) + abs(ay - by)
     return distances
 
-IMAGE = get_puzzle_input(r"./puzzle_input.txt")
-ROWS, COLS = get_empty_spaces(IMAGE)
+UNIVERSE = get_puzzle_input(r"./puzzle_input.txt")
+ROWS, COLS = get_empty_spaces(UNIVERSE)
 
-GALAXIES = get_galaxies(IMAGE, ROWS, COLS, 2)  # part 1
-GALAXIES = get_galaxies(IMAGE, ROWS, COLS, 1000000)  # part 2
+GALAXIES = get_galaxies(UNIVERSE, ROWS, COLS, 2)  # part 1
+GALAXIES = get_galaxies(UNIVERSE, ROWS, COLS, 1000000)  # part 2
 
 print(get_sum_distances(GALAXIES))
