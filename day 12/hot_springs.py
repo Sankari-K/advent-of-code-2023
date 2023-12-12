f1db04d5be05fc8805e8420c55636aba1f01a9dc
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-12 22:03:55 +0530

    Add optimzed soln for day 12

--- day 12/hot_springs.py
+++ day 12/hot_springs.py
@@ -18,12 +18,12 @@ def modify_springs(springs):
 @lru_cache
 def get_arrangement(spring_pos, current_pos, matched_damaged_count, current_damaged_count):
     spring, schema = SPRINGS[spring_pos][0] + ".", SPRINGS[spring_pos][1]
-    
+    # base case
     if current_pos == len(spring):
         if matched_damaged_count == len(schema):
             return 1 if current_damaged_count == 0 else 0
         return 0
-    
+    # conditions to exit early
     if matched_damaged_count == len(schema):
         if current_damaged_count != 0 or "#" in spring[current_pos:]:
             return 0
@@ -32,12 +32,12 @@ def get_arrangement(spring_pos, current_pos, matched_damaged_count, current_dama
         return 0
     if current_damaged_count > schema[matched_damaged_count]:
         return 0 
-    
+    # recursive case
     ans = 0
     if spring[current_pos] in "#?":
         ans += get_arrangement(spring_pos, current_pos + 1, matched_damaged_count, current_damaged_count + 1)
     if spring[current_pos] in ".?":
-        if current_damaged_count != 0 and matched_damaged_count < len(schema) and schema[matched_damaged_count] == current_damaged_count:
+        if current_damaged_count != 0 and schema[matched_damaged_count] == current_damaged_count:
             ans += get_arrangement(spring_pos, current_pos + 1, matched_damaged_count + 1, 0)
         elif current_damaged_count == 0:
             ans += get_arrangement(spring_pos, current_pos + 1, matched_damaged_count, current_damaged_count)
@@ -46,6 +46,6 @@ def get_arrangement(spring_pos, current_pos, matched_damaged_count, current_dama
 def get_sum_arrangements(SPRINGS):
     return sum([get_arrangement(spring_pos, 0, 0, 0) for spring_pos in range(len(SPRINGS))])
 
-SPRINGS = get_puzzle_input(r"./input.txt")
+SPRINGS = get_puzzle_input(r"./puzzle_input.txt")
 SPRINGS = modify_springs(SPRINGS)
 print(get_sum_arrangements(SPRINGS))
