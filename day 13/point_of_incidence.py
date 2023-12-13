cd974bce63fe89aa95210375ebc37d58fe0aef01
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-13 21:08:57 +0530

    Remove unnecessary conditions while finding the reflection

--- day 13/point_of_incidence.py
+++ day 13/point_of_incidence.py
@@ -3,31 +3,25 @@ def get_puzzle_input(directory):
         patterns = file.read().split("\n\n")
         return [pattern.split("\n") for pattern in patterns]
 
-def get_horizontal_reflection_line(pattern, smudges_exist):
+def get_horizontal_reflection_line(pattern, smudge_limit):
     for horizontal_line in range(1, len(pattern)):
-        top, bottom = pattern[:horizontal_line], pattern[horizontal_line:]
-        min_length = min(len(top), len(bottom))
-        top, bottom = top[-1 * min_length:], bottom[:min_length][::-1]
-
-        if smudges_exist:
-            smudges = 0
-            for top_row, bottom_row in zip(top, bottom):
-                for top_char, bottom_char in zip(top_row, bottom_row):
-                    if top_char != bottom_char:
-                        smudges += 1
-            if smudges == 1:
-                return horizontal_line 
-        elif top == bottom:
-            return horizontal_line
+        top, bottom = pattern[:horizontal_line][::-1], pattern[horizontal_line:]
+        smudges = 0
+        for top_row, bottom_row in zip(top, bottom):
+            for top_char, bottom_char in zip(top_row, bottom_row):
+                if top_char != bottom_char:
+                    smudges += 1
+        if smudges == smudge_limit:
+            return horizontal_line 
     return 0
 
-def get_summarized_notes(NOTES, smudges_exist=False):
+def get_summarized_notes(NOTES, smudge_limit):
     ans = 0
     for pattern in NOTES:
-        ans += 100 * get_horizontal_reflection_line(pattern, smudges_exist)
-        ans += get_horizontal_reflection_line(list(zip(*pattern)), smudges_exist)
+        ans += 100 * get_horizontal_reflection_line(pattern, smudge_limit)
+        ans += get_horizontal_reflection_line(list(zip(*pattern)), smudge_limit)
     return ans
 
 NOTES = get_puzzle_input(r"./puzzle_input.txt")
-print(get_summarized_notes(NOTES))
-print(get_summarized_notes(NOTES, True))
+print(get_summarized_notes(NOTES, 0))
+print(get_summarized_notes(NOTES, 1))
