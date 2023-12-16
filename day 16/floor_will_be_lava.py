6673b62739519bbcefe28e842a6bb5298627e1e1
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-16 13:34:18 +0530

    Add cleaner soln for day 16

--- day 16/floor_will_be_lava.py
+++ day 16/floor_will_be_lava.py
@@ -1,5 +1,31 @@
 from collections import deque
 
+DIRECTIONS = {"-": {"T": ["L", "R"], "D": ["L", "R"], "L": ["R"], "R": ["L"]},
+              "|": {"T": ["D"], "D": ["T"], "L": ["T", "D"], "R": ["T", "D"]},
+              ".": {"T": ["D"], "D": ["T"], "L": ["R"], "R": ["L"]},
+              "/": {"T": ["L"], "D": ["R"], "L": ["T"], "R": ["D"]},
+              "\\": {"T": ["R"], "D": ["L"], "L": ["D"], "R": ["T"]}}
+
+def get_relative_direction(prev, current):
+    x, y = current
+    if prev == (x, y - 1):
+        return "L"
+    if prev == (x, y + 1):
+        return "R"
+    if prev == (x - 1, y):
+        return "T"
+    return "D"
+
+def get_absolute_direction(current, direction):
+    x, y = current
+    if direction == "T":
+        return (x - 1, y)
+    if direction == "D":
+        return (x + 1, y)
+    if direction == "L":
+        return (x, y - 1)
+    return (x, y + 1)
+
 def get_puzzle_input(directory):
     LAYOUT = dict()
     MAX_X = MAX_Y = 0
@@ -12,74 +38,32 @@ def get_puzzle_input(directory):
                 LAYOUT[(x, y)] = char
     return LAYOUT, MAX_X, MAX_Y
 
-def get_energized_tiles(prev, x, y):
+def get_energized_tiles(prev, current):
     queue = deque()
-    queue.append([prev, x, y])
+    queue.append([prev, current])
     seen = set()
 
     while queue:
-        prev, x, y = queue.popleft()
-        if (x, y) not in LAYOUT or (prev, x, y) in seen:
+        prev, current = queue.popleft()
+        if current not in LAYOUT or (prev, current) in seen:
             continue
-        seen.add((prev, x, y))
-
-        # if previous is from the left
-        if prev == (x, y - 1):
-            if LAYOUT[(x, y)] in "-.":
-                queue.append([(x, y), x, y + 1])
-            elif LAYOUT[(x, y)] == "|":
-                queue.append([(x, y), x - 1, y])
-                queue.append([(x, y), x + 1, y])
-            elif LAYOUT[(x, y)] == "/":
-                queue.append([(x, y), x - 1, y])
-            elif LAYOUT[(x, y)] == "\\":
-                queue.append([(x, y), x + 1, y])
-        # if previous is from the right
-        elif prev == (x, y + 1):
-            if LAYOUT[(x, y)] in "-.":
-                queue.append([(x, y), x, y - 1])
-            elif LAYOUT[(x, y)] == "|":
-                queue.append([(x, y), x - 1, y])
-                queue.append([(x, y), x + 1, y])
-            elif LAYOUT[(x, y)] == "/":
-                queue.append([(x, y), x + 1, y])
-            elif LAYOUT[(x, y)] == "\\":
-                queue.append([(x, y), x - 1, y])
-        # if previous is from the top
-        elif prev == (x - 1, y):
-            if LAYOUT[(x, y)] in "|.":
-                queue.append([(x, y), x + 1, y])
-            elif LAYOUT[(x, y)] == "-":
-                queue.append([(x, y), x, y + 1])
-                queue.append([(x, y), x, y - 1])
-            elif LAYOUT[(x, y)] == "/":
-                queue.append([(x, y), x, y - 1])
-            elif LAYOUT[(x, y)] == "\\":
-                queue.append([(x, y), x, y + 1])
-        # if previous is from the bottom
-        elif prev == (x + 1, y):
-            if LAYOUT[(x, y)] in "|.":
-                queue.append([(x, y), x - 1, y])
-            elif LAYOUT[(x, y)] == "-":
-                queue.append([(x, y), x, y + 1])
-                queue.append([(x, y), x, y - 1])
-            elif LAYOUT[(x, y)] == "/":
-                queue.append([(x, y), x, y + 1])
-            elif LAYOUT[(x, y)] == "\\":
-                queue.append([(x, y), x, y - 1])
-
-    return len(set(s[1:] for s in seen))
+        seen.add((prev, current)) 
+        possible_directions = DIRECTIONS[LAYOUT[current]][get_relative_direction(prev, current)]
+        for direction in possible_directions:
+            queue.append([current, get_absolute_direction(current, direction)])
+       
+    return len(set(s[1] for s in seen))
 
 def get_max_energized_tiles(MAX_X, MAX_Y):
     max_tiles = 0
     for y in range(MAX_Y):
-        max_tiles = max(get_energized_tiles((-1, y), 0, y), max_tiles)
-        max_tiles = max(get_energized_tiles((MAX_X, y), MAX_X - 1, y), max_tiles)
+        max_tiles = max(get_energized_tiles((-1, y), (0, y)), max_tiles)
+        max_tiles = max(get_energized_tiles((MAX_X, y), (MAX_X - 1, y)), max_tiles)
     for x in range(MAX_X):
-        max_tiles = max(get_energized_tiles((x, -1), x, 0), max_tiles)
-        max_tiles = max(get_energized_tiles((x, MAX_Y), x, MAX_Y - 1), max_tiles)
+        max_tiles = max(get_energized_tiles((x, -1), (x, 0)), max_tiles)
+        max_tiles = max(get_energized_tiles((x, MAX_Y), (x, MAX_Y - 1)), max_tiles)
     return max_tiles
 
 LAYOUT, MAX_X, MAX_Y = get_puzzle_input(r"./puzzle_input.txt")
-print(get_energized_tiles((0, -1), 0, 0)) 
+print(get_energized_tiles((0, -1), (0, 0))) 
 print(get_max_energized_tiles(MAX_X, MAX_Y))
