5ae7ec5fc465e6aac0299200d19466976a4aadce
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-17 20:12:24 +0530

    Add working soln for day 17

--- day 17/clumsy_crucible.py
+++ day 17/clumsy_crucible.py
@@ -35,16 +35,15 @@ def get_relative_direction(prev, current):
         return "D"
     return "T"
 
-def get_min_heat_loss():
+def get_min_heat_loss(min_direction, max_direction):
     seen = set()
     queue = [(0, (0, 1), (0, 0), 1), (0, (1, 0), (0, 0), 1)]
 
     while queue:
         loss, current, prev, direction_steps = heapq.heappop(queue)
 
-        # if current is the destination (bottom-right)
-        # if current == (MAX_X - 1, MAX_Y - 1):
-        #     return loss
+        if prev == (MAX_X - 1, MAX_Y - 1):
+            return loss
 
         # if out of bounds
         if current not in CITY:
@@ -55,18 +54,16 @@ def get_min_heat_loss():
     
         for direction in DIRECTIONS:
             next = get_next(current, direction)
+
             if next == prev:
                 continue
 
-            if direction == get_relative_direction(prev, current) and direction_steps < 3:
-                if next == (MAX_X - 1, MAX_Y - 1):
-                    return loss + CITY[(MAX_X - 1, MAX_Y - 1)] + CITY[current]
-
+            if direction == get_relative_direction(prev, current) and direction_steps < max_direction:
                 heapq.heappush(queue, (loss + CITY[current], next, current, direction_steps + 1))
-            elif direction != get_relative_direction(prev, current):
-                if get_next(current, direction) == (MAX_X - 1, MAX_Y - 1):
-                    return loss + CITY[(MAX_X - 1, MAX_Y - 1)]
+            elif direction != get_relative_direction(prev, current) and direction_steps >= min_direction:
                 heapq.heappush(queue, (loss + CITY[current], next, current, 1))
   
 CITY, MAX_X, MAX_Y = get_puzzle_input(r"./puzzle_input.txt")
-print(get_min_heat_loss())
+print(get_min_heat_loss(1, 3))
+print(get_min_heat_loss(4, 10))
+
