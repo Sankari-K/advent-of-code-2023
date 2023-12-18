08854b6c6e5bcce5cb1da79504af899d11bbd1d4
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-18 13:10:43 +0530

    Add solution for day 18

--- day 18/lavaduct_lagoon.py
+++ day 18/lavaduct_lagoon.py
@@ -1,31 +1,43 @@
-from matplotlib.path import Path
-
-NEXT = {"U": lambda x, y, a: [(x - i, y) for i in range(1, a + 1)], 
-        "D": lambda x, y, a: [(x + i, y) for i in range(1, a + 1)], 
-        "L": lambda x, y, a: [(x, y - i) for i in range(1, a + 1)], 
-        "R": lambda x, y, a: [(x, y + i) for i in range(1, a + 1)]}
+NEXT = {"U": lambda x, y, a: (x - a, y), "D": lambda x, y, a: (x + a, y), "L": lambda x, y, a: (x, y - a), "R": lambda x, y, a: (x, y + a)}
+DIRECTION = {"0": "R", "1": "D", "2": "L", "3": "U"}
 
 def get_puzzle_input(directory):
     with open(directory) as file:
         return [line.strip().split() for line in file]
+    
+def modify_dig_plan(DIG_PLAN, part):
+    if part == "a":
+        return [[direction, steps] for direction, steps, color in DIG_PLAN]
+    
+    for index, plan in enumerate(DIG_PLAN):
+        hexdecimal = plan[-1][1:-1][1:]
+        DIG_PLAN[index] = [DIRECTION[hexdecimal[-1]], int(hexdecimal[:-1], 16)]
+    return DIG_PLAN
 
-def dig_perimeter(DIG_PLAN):
+
+def dig_boundary(DIG_PLAN):
     current = (0, 0)
     coordinates = [current]
 
-    for direction, steps, color in DIG_PLAN:
+    for direction, steps in DIG_PLAN:
         current = NEXT[direction](*current, int(steps))
-        coordinates.extend(current)
-        current = current[-1]
+        coordinates.append(current)
 
-    return coordinates[:-1]
+    return coordinates
 
-def get_area(path):    
-    area = 0
-    for i, point in enumerate(path[1:-1], start=1):
-        area += point[1] * (path[i - 1][0] - path[i + 1][0])
-    area = 0.5 * abs(area)
-    return area + 1 - len(path)//2 + len(path)
+def get_lava_capacity(path):   
+    area, path_pts = 0, 0
 
-DIG_PLAN = get_puzzle_input(r"./puzzle_input.txt")
-print(get_area(dig_perimeter(DIG_PLAN)))
+    for a, b in zip(path, path[1:]):
+        area += (a[0] * b[1]) - (a[1] * b[0])
+        path_pts += abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+    area = abs(area // 2)
+    interior_pts = area + 1 - path_pts // 2
+    return interior_pts + path_pts
+
+DIG_PLAN = modify_dig_plan(get_puzzle_input(r"./puzzle_input.txt"), "a")
+print(get_lava_capacity(dig_boundary(DIG_PLAN)))
+
+DIG_PLAN = modify_dig_plan(get_puzzle_input(r"./puzzle_input.txt"), "b")
+print(get_lava_capacity(dig_boundary(DIG_PLAN)))
