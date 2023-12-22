71b7bd1cfbbda66488da7f2ed65868844dbacbb5
Sankari Karthik <sankarikarthik03#gmail.com>
2023-12-22 17:28:52 +0530

    Add half working messy soln

--- day 22/sand_slabs.py
+++ day 22/sand_slabs.py
@@ -1,13 +1,17 @@
+from collections import defaultdict, deque
+
 def get_puzzle_input(directory):
     with open(directory) as file:
         file = file.read().split("\n")
     
     BRICKS = dict()
     MAX_DIMENSIONS = [-1, -1, -1]
+
     for index, line in enumerate(file):
         old, new = line.strip().split("~")
         old = list(map(int, old.split(",")))
         new = list(map(int, new.split(",")))
+
         for x in range(old[0], new[0] + 1):
             for y in range(old[1], new[1] + 1):
                 for z in range(old[2], new[2] + 1):
@@ -16,24 +20,18 @@ def get_puzzle_input(directory):
     return BRICKS, MAX_DIMENSIONS
 
 def get_bricks_given_height(z):
-    req_bricks = set()
-    for brick in BRICKS:
-        if brick[2] == z:
-            req_bricks.add(BRICKS[brick])
-    return req_bricks
+    return set([BRICKS[brick] for brick in BRICKS if brick[2] == z])
 
 def get_all_coordinates(brick_id):
     coordinates = set()
     for brick_pos, brick_num in BRICKS.items():
-        if brick_num == brick_id:
+        if brick_id == brick_num:
             coordinates.add(brick_pos)
     return coordinates
 
 def simulate_entire_fall(BRICKS, MAX_Z):
-    for level in range(1, MAX_Z + 1):
-        # find all brick types in that level
-        all_bricks = get_bricks_given_height(level)
-        for brick in all_bricks:
+    for level in range(1, MAX_Z + 1): # find all brick types in that level
+        for brick in get_bricks_given_height(level):
             simulate_fall_brick(brick, level)
     return BRICKS
         
@@ -42,7 +40,7 @@ def simulate_fall_brick(brick, level):
     xyplane = set([(coordinate[0], coordinate[1]) for coordinate in coordinates])
 
     lower = 1
-    while all([((coordinate[0], coordinate[1], level - lower) not in BRICKS) and (level - lower >= 1) for coordinate in xyplane]):
+    while all([(level - lower >= 1) and ((coordinate[0], coordinate[1], level - lower) not in BRICKS) for coordinate in xyplane]):
         lower += 1
     lower -= 1
 
@@ -51,9 +49,51 @@ def simulate_fall_brick(brick, level):
         BRICKS[(coordinate[0], coordinate[1], coordinate[2] - lower)] = brick
 
 def get_support_stats(BRICKS):
+    support_stats = defaultdict(set)
     for brick_id in set(BRICKS.values()):
-        print(brick_id)
+        coordinates = get_all_coordinates(brick_id)
+        xyplane = set([(coordinate[0], coordinate[1]) for coordinate in coordinates])
+        max_z = max([coordinate[2] for coordinate in coordinates])
+
+        for x, y in xyplane:
+            if (x, y, max_z + 1) in BRICKS:
+                support_stats[brick_id].add(BRICKS[(x, y, max_z + 1)])
+
+    return support_stats
+
+def get_safe_bricks(support_stats):
+    can_be_disintegrated = 0
+    for brick_id, supporting_bricks in support_stats.items():
+       other_values = set([s for brick, supports in support_stats.items() for s in supports  if brick != brick_id])
+       if len(supporting_bricks) - len(other_values.intersection(set(supporting_bricks))) == 0:
+           can_be_disintegrated += 1
+    
+    return can_be_disintegrated + len(set(BRICKS.values())) - len(support_stats)
+
+def get_brick_score(brick):
+    seen = set([brick])
+    queue = deque([brick])
+
+    while queue:
+        next_brick = queue.popleft()
+        other_values = set([s for brick, supports in SUPPORT_STATS.items() for s in supports if brick not in seen])
+
+        for support_brick in SUPPORT_STATS[next_brick]:
+            if support_brick not in seen and support_brick not in other_values:
+                seen.add(support_brick)
+                queue.append(support_brick)
+    return len(seen) - 1
+
+def get_sum_fallen_bricks(support_stats):
+    ans = 0
+    for brick_id in list(support_stats.keys()):
+        ans += get_brick_score(brick_id)
+    return ans
 
 BRICKS, MAX_DIMENSIONS = get_puzzle_input(r"./input.txt")
 BRICKS = simulate_entire_fall(BRICKS, MAX_DIMENSIONS[-1])
-get_support_stats(BRICKS)
+SUPPORT_STATS = get_support_stats(BRICKS)
+
+print(SUPPORT_STATS)
+# print(get_safe_bricks(SUPPORT_STATS))
+print(get_sum_fallen_bricks(SUPPORT_STATS))
